a32dee7bd6a5e94301b1e6ce01973aec52036b2d
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-23 18:10:41 +0530

    countState

--- reactcounter/src/assets/components/counter.jsx
+++ reactcounter/src/assets/components/counter.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const counter = () => {
+     const [count, setCount] = React.useState(0);
+
+    const handleIncrement = () => {
+      setCount(count + 1);
+    };  
   return (
+   
     <div className='counter'>
-        <p id='counter-value'>you have clicked 0 times</p>
-        <button id='increment-btn'>Increment</button>    
+        <p id='counter-value'>you have clicked {count} times</p>
+        <button id='increment-btn' onClick={handleIncrement}>
+          Increment
+        </button>    
       
     </div>
   )
