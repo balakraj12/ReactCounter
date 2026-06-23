3468456f7a8d6fe708a0eb3fa0658da78422e31b
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-23 17:59:13 +0530

    added counter

---
+++ reactcounter/src/assets/components/counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const counter = () => {
+  return (
+    <div className='counter'>
+        <p id='counter-value'>you have clicked 0 times</p>
+      
+    </div>
+  )
+}
+
+export default counter
