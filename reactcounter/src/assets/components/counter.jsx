6764b3eb0bc1ca6831f13d8e1b586efa7e30163d
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-23 18:00:38 +0530

    increment button added in component counter file

--- reactcounter/src/assets/components/counter.jsx
+++ reactcounter/src/assets/components/counter.jsx
@@ -4,6 +4,7 @@ const counter = () => {
   return (
     <div className='counter'>
         <p id='counter-value'>you have clicked 0 times</p>
+        <button id='increment-btn'>Increment</button>    
       
     </div>
   )
